4ef1c16e14eccc14acc0d21bb9328a762adad63a
rithanyaa42-ops <rithanyaa42@gmail.com>
2025-12-30 11:44:13 +0530

    Update half ad gl.v

--- half ad gl.v
+++ half ad gl.v
@@ -1,1 +1,14 @@
+module half_ad_gl(
+input wire a,b,
+output reg sum,carry);
+always @(*) begin
+case({a,b})
+2'b00:begin sum=0;carry=0;end
+2'b00:begin sum=1;carry=0;end
+2'b00:begin sum=1;carry=0;end
+2'b00:begin sum=0;carry=1;end
+endcase
+end
+endmodule
+
 
